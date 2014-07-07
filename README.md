5b8d2d787ee888d6965f765ea69bd4247e4b9e89
Benedikt Gross <benedikt@looksgood.de>
2014-07-07 13:41:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+mini-mbtiles-server
+===================
+
+1. `npm install`
+2. Run `node server.js`
+3. Copy a .mbtiles file in the root directory, e.g.: ./abc.mbtiles
+3. The tiles are served via http://localhost:3000/abc/3/1/2.png
+
+Based on Tobin Bradley's nice [tutorial](https://www.youtube.com/watch?v=CwAQSKsSQhI) on [mbtiles-server](https://github.com/tobinbradley/mbtiles-server) with a few minor modifications.
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express"),
+  app = express(),
+  MBTiles = require('mbtiles'),
+  p = require("path");
+
+// path to the mbtiles; default is the server.js directory
+var tilesDir = __dirname;
+
+app.get('/', function(req, res){
+  res.send('hello! mini-mbtiles-server bellow');
+});
+
+app.get('/:s/:z/:x/:y.*', function(req, res) {
+  new MBTiles(p.join(tilesDir, req.param('s') + '.mbtiles'), function(err, mbtiles) {
+    mbtiles.getTile(req.param('z'), req.param('x'), req.param('y'), function(err, tile, headers) {
+      if (err) {
+        res.set({
+          "Content-Type": "text/plain"
+        });
+        res.status(404).send('Tile rendering error: ' + err + '\n');
+      } else {
+        res.set({
+          "Content-Type": "image/png",
+          // "Cache-Control": "public, max-age=2592000"  // leave this out for no caching - default is 1 month
+        });
+        res.send(tile);
+      }
+    });
+    if (err) console.log("error opening database");
+  });
+});
+
+var server = app.listen(3000, function() {
+  console.log('Serving HTTP on http://localhost:%d/', server.address().port);
+});
