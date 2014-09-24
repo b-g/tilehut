4064d38e056a16a9c2567e487763a96f4410e89c
Benedikt Gross <benedikt@looksgood.de>
2014-09-24 14:25:09 +0200

    refactoring, middleware to check whether mbtile file exists

--- server.js
+++ server.js
@@ -10,59 +10,50 @@ var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_P
   , TILES_DIR = process.env.OPENSHIFT_DATA_DIR || __dirname
   ;
 
+var mbtilesfile = null;
+
 app.get('/', function(req, res){
   res.send('hello! tilehut bellow');
 });
 
-app.get('/:s/:z/:x/:y.*', function(req, res) {
-  var mbtilesfile = path.join(TILES_DIR, req.param('s') + '.mbtiles');
+// middleware to check whether mbtilesfile exists
+app.use('/:s', function(req, res, next) {
+  mbtilesfile = path.join(TILES_DIR, req.param('s') + '.mbtiles');
   fs.exists(mbtilesfile, function (exists) {
     if (!exists) {
-      console.error("mbtiles file doesn't exist -> path: "+mbtilesfile);
-      return res.sendStatus(404);
+      return internalError(null, req, res, 404, "cannot initialize MBTiles object");
     }
-    new MBTiles(mbtilesfile, function(err, mbtiles) {
-      if (err) {
-        console.error("cannot initialize MBTiles object -> "+err);
-        return res.sendStatus(500);
-      }
-      mbtiles.getTile(req.param('z'), req.param('x'), req.param('y'), function(err, tile, headers) {
-        if (err) {
-          console.error('tile rendering error -> url: ' + req.originalUrl + ', error: ' + err);
-          return res.sendStatus(404);
-        }
-        res.set(headers);
-        res.send(tile);
-      });
+    next();
+  });
+});
+
+app.get('/:s/:z/:x/:y.*', function(req, res) {
+  new MBTiles(mbtilesfile, function(err, mbtiles) {
+    if (err) { internalError(err, req, res, 500, "cannot initialize MBTiles object") };
+    mbtiles.getTile(req.param('z'), req.param('x'), req.param('y'), function(err, tile, headers) {
+      if (err) { internalError(err, req, res, 404, "tile rendering error") };
+      res.set(headers);
+      res.send(tile);
     });
   });
 });
 
 app.get('/:s/metadata.json', function(req, res) {
-  var mbtilesfile = path.join(TILES_DIR, req.param('s') + '.mbtiles');
-  fs.exists(mbtilesfile, function (exists) {
-    if (!exists) {
-      console.error("mbtiles file doesn't exist -> path: "+mbtilesfile);
-      return res.sendStatus(404);
-    }
-    new MBTiles(mbtilesfile, function(err, mbtiles) {
-      if (err) {
-        console.error("cannot initialize MBTiles object -> "+err);
-        return res.sendStatus(500);
-      }
-      mbtiles.getInfo(function(err, info) {
-        if (err) {
-          console.error('tile rendering error -> url: ' + req.originalUrl + ', error: ' + err);
-          return res.sendStatus(404);
-        }
-        res.send(info);
-      });
+  new MBTiles(mbtilesfile, function(err, mbtiles) {
+    if (err) { internalError(err, req, res, 500, "cannot initialize MBTiles object") };
+    mbtiles.getInfo(function(err, info) {
+      if (err) { internalError(err, req, res, 404, "cannot get metadata") };
+      res.send(info);
     });
   });
 });
 
 var server = require('http').createServer(app);
 server.listen(PORT, IPADDRESS, function() {
-  var url = "http://" + server.address().address +":"+ server.address().port;
-  console.log('Serving HTTP on ', url);
+  console.log('Tilehut on http://%s:%s', server.address().address, server.address().port);
 });
+
+function internalError(err, req, res, statusNum, errorMsg) {
+  console.error('%s %s %s', req.url, errorMsg, err);
+  return res.sendStatus(statusNum);
+};
