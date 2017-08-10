76aa778122d09db73d29f1c39d10512856feb0ea
Joey Lee <josephkanglee@gmail.com>
2017-08-10 15:23:26 +0200

    changed config for heroku deployment
    named tiles_dir to MY_DATA_DIR in case people want to change it, but I
    don’t want to change things too much here.

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 var path = require('path');
 
 var config = {
-  PORT: process.env.OPENSHIFT_NODEJS_PORT || 8000,
-  IPADDRESS: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  TILES_DIR: process.env.OPENSHIFT_DATA_DIR || path.join(__dirname, '/data'),
+  PORT: process.env.PORT || 8000,
+  IPADDRESS: process.env.IP || '0.0.0.0',
+  TILES_DIR: process.env.MY_DATA_DIR || path.join(__dirname, '/data'),
   MAP_DIR: path.join(__dirname, '/static/map'),
 }
 
